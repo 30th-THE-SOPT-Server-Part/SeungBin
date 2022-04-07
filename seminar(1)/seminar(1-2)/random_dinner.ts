e59319a56c40cf2999543969d439422e35c770ff
csb9427 <csb9427@naver.com>
2022-04-08 00:14:06 +0900

    second task completion

---
+++ seminar(1)/seminar(1-2)/random_dinner.ts
@@ -0,0 +1,59 @@
+// 과제 조건
+// 1. Member, Dinner interface 만들고 타입 지정하기
+// 2. organize 내부 로직 채우기
+
+
+interface Member {
+    name: string;
+    group: string;
+}
+
+interface Dinner {
+    member: Member[];
+    shuffle(array: Member[]): Member[];
+    organize(array: Member[]): void;
+}
+
+const dinner: Dinner = {
+
+    member: [
+        {
+            name: '채정아',
+            group: 'ob'
+        },
+        {
+            name: '최승빈',
+            group: 'yb'
+        },
+        {
+            name: '박정훈',
+            group: 'ob'
+        },
+        {
+            name: '김희빈',
+            group: 'ob'
+        },
+        {
+            name: '박진수',
+            group: 'yb'
+        },
+        {
+            name: '임승하',
+            group: 'yb'
+        }
+    ],
+
+    shuffle(array){
+        array.sort(() => Math.random() - 0.5);
+        return array;
+    },
+
+    organize(array) {
+        this.shuffle(array);
+        let ob: Member = array.filter((member: Member) => (member.group === 'ob'))[0];
+        let yb: Member = array.filter((member: Member) => (member.group ==='yb'))[0];
+        let dinnerMember: string[] = [ob.name, yb.name];
+       
+        console.log(`오늘의 저녁 식사 멤버는 ${dinnerMember[0]}, ${dinnerMember[1]}입니다.`);
+    }
+};
